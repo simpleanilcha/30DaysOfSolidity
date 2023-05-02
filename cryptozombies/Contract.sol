61575ced1d5f5077f68ad1fdb7a14dc1e4cacd90
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-02 23:40:07 +0545

    Declare and Fire event

--- cryptozombies/Contract.sol
+++ cryptozombies/Contract.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.19;
 // Initialize contract
 contract ZombieFactory {
 
+    // declare event
+    event NewZombie(uint zombieId, string name, uint dna);
+
     // State variables & Integers
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -19,11 +22,20 @@ contract ZombieFactory {
 
     // Functions
     function _createZombie(string memory _name, uint _dna) private {
+        // Fire and event
         zombies.push(Zombie(_name, _dna));
+        uint id = zombies.length - 1;
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
 
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 
 }
