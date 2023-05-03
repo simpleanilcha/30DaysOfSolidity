df4413bfe2259e8318a56222ab8eb9eede86ce95
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-03 23:44:35 +0545

    Storage vs Memory (Data location)

--- cryptozombies/L2_zombiefeeding.sol
+++ cryptozombies/L2_zombiefeeding.sol
@@ -4,5 +4,10 @@ pragma solidity 0.8.19;
 import "./L2_zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
-    
+  
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+  }
+
 }
