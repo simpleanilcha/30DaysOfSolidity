5915155ea01caed049288d601621aad62e784609
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-03 22:20:50 +0545

    Lesson 2 started with Mappings and Addresses data types

---
+++ cryptozombies/L2_Contract.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract ZombieFactory {
+
+    // declare event
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    // State variables & Integers
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    // Structs
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    // Arrays
+    Zombie[] public zombies;
+
+    // mappings here
+    mapping(uint => address) public zombieToOwner;
+    mapping(address => uint) ownerZombieCount;
+
+    // Functions
+    function _createZombie(string memory _name, uint _dna) private {
+        // Fire and event
+        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.length - 1;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}
