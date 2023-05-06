c02072a0f3f925eb1e18aad0ef4ab72aa18d1270
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-06 21:11:28 +0545

    3.09 Zombie Modifiers

--- cryptozombies/L3_zombiehelper.sol
+++ cryptozombies/L3_zombiehelper.sol
@@ -5,10 +5,20 @@ import "./L3_zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
-  // Start here
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
+  }
+
+  // Start here
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].name = _newName;
+  }
+
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].dna = _newDna;
   }
 
 }
