554a19d674d45c315e8a0d62571960621fc5846d
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-07 23:04:25 +0545

    4.07 More Refactoring

--- cryptozombies/L4_Zombie_Battle_System/zombiehelper.sol
+++ cryptozombies/L4_Zombie_Battle_System/zombiehelper.sol
@@ -5,7 +5,6 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
-  // 1. Define levelUpFee here
   uint levelUpFee = 0.001 ether;
 
   modifier aboveLevel(uint _level, uint _zombieId) {
@@ -13,30 +12,29 @@ contract ZombieHelper is ZombieFeeding {
     _;
   }
 
-  // 1. Create withdraw function here
   function withdraw() external onlyOwner {
     address payable _owner = address(uint160(owner()));
     _owner.transfer(address(this).balance);
   }
 
-  // 2. Create setLevelUpFee function here
   function setLevelUpFee(uint _fee) external onlyOwner {
     levelUpFee = _fee;
   }
 
-  // 2. Insert levelUp function here
   function levelUp(uint _zombieId) external payable {
     require(msg.value == levelUpFee);
     zombies[_zombieId].level++;
   }
 
-  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  // 1. Modify this function to use `ownerOf`:
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId)  ownerOf(_zombieId) {
+    // require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].name = _newName;
   }
 
-  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  // 2. Do the same with this function:
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) ownerOf(_zombieId) {
+    // require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].dna = _newDna;
   }
 
