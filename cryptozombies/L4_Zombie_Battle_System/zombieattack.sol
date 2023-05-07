e4f31a781476995d5d989f66fd68c527fbfd10af
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-07 22:36:11 +0545

    4.04 Random Numbers

--- cryptozombies/L4_Zombie_Battle_System/zombieattack.sol
+++ cryptozombies/L4_Zombie_Battle_System/zombieattack.sol
@@ -4,5 +4,11 @@ pragma solidity 0.8.19;
 import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
+  // Start here
+  uint randNonce = 0;
 
+  function randMod(uint _modulus) internal returns (uint) {
+    randNonce++;
+    return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % _modulus;
+  }
 }
