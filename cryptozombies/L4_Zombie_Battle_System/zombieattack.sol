fc33110938e18c417529dd58273c78ed8ab33638
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-07 22:43:46 +0545

    4.05 Zommbie Fighting

--- cryptozombies/L4_Zombie_Battle_System/zombieattack.sol
+++ cryptozombies/L4_Zombie_Battle_System/zombieattack.sol
@@ -4,11 +4,17 @@ pragma solidity 0.8.19;
 import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
-  // Start here
   uint randNonce = 0;
+  // Create attackVictoryProbability here
+  uint attackVictoryProbability = 70;
 
   function randMod(uint _modulus) internal returns (uint) {
     randNonce++;
     return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % _modulus;
+  }
+
+  // Create new function here
+  function attack(uint _zombieId, uint _targetId) external {
+    
   }
 }
