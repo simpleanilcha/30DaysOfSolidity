1c567be55f50f5fe16772e9d871f6ecf56dcbca5
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-11 23:04:40 +0545

    5.06 Transfer Continued

--- cryptozombies/L5_ERC21_&_Crypto-Collectibles/zombieownership.sol
+++ cryptozombies/L5_ERC21_&_Crypto-Collectibles/zombieownership.sol
@@ -7,6 +7,9 @@ import "./erc721.sol";
 // Declare ERC721 inheritance here
 abstract contract ZombieOwnership is ZombieAttack, ERC721 {
 
+  // 1. Define mapping here
+  mapping (uint => address) zombieApprovals;
+
   function balanceOf(address _owner) external view returns (uint256) {
     return ownerZombieCount[_owner];
   }
@@ -24,7 +27,10 @@ abstract contract ZombieOwnership is ZombieAttack, ERC721 {
   }
 
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-
+    // 2. Add the require statement here
+    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+    // 3. Call _transfer
+    _transfer(_from, _to, _tokenId);
   }
 
   function approve(address _approved, uint256 _tokenId) external payable {
