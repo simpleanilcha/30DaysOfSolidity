ab82fd46858bc434827f71bd941ee0011dd803a8
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-13 00:46:57 +0545

    5.11 Safemath Part 3

--- cryptozombies/L5_ERC21_&_Crypto-Collectibles/zombiefactory.sol
+++ cryptozombies/L5_ERC21_&_Crypto-Collectibles/zombiefactory.sol
@@ -2,13 +2,15 @@
 pragma solidity 0.8.19;
 
 import "./ownable.sol";
-// 1. Import here
 import "./safemath.sol";
 
 contract ZombieFactory is Ownable {
 
-    // 2. Declare using safemath here
     using SafeMath for uint256;
+    // 1. Declare using SafeMath32 for uint32
+    using SafeMath32 for uint32;
+    // 2. Declare using SafeMath16 for uint16
+    using SafeMath16 for uint16;
 
     event NewZombie(uint zombieId, string name, uint dna);
 
@@ -31,10 +33,13 @@ contract ZombieFactory is Ownable {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) internal {
+        // Note: We chose not to prevent the year 2038 problem... So don't need
+        // worry about overflows on readyTime. Our app is screwed in 2038 anyway ;)
         zombies.push(Zombie(_name, _dna, 1, uint32(block.timestamp + cooldownTime), 0, 0));
         uint id = zombies.length - 1;
         zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;
+        // 3. Let's use SafeMath's `add` here:
+        ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
         emit NewZombie(id, _name, _dna);
     }
 
