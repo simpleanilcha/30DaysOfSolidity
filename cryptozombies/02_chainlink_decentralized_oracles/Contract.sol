d37cb481c1774909ab084b8426ce49fdfb5c1121
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-18 04:20:01 +0545

    AggregatorV3Interface

--- cryptozombies/02_chainlink_decentralized_oracles/Contract.sol
+++ cryptozombies/02_chainlink_decentralized_oracles/Contract.sol
@@ -2,6 +2,14 @@
 pragma solidity ^0.6.7;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
-contract PriceConsumerV3 {
 
+contract PriceConsumerV3 {
+  // 1. Create a `public` variable named `priceFeed` of type `AggregatorV3Interface`.
+  AggregatorV3Interface public priceFeed;
+
+  // 2. Create a constructor
+  constructor() public {
+    // 3. Instantiate the `AggregatorV3Interface` contract
+    priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+  }
 }
