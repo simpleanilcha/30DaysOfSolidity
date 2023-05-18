701a64f2033adbc92baf8206a95204c61605c12e
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-19 05:30:13 +0545

    Chainlink Data Feeds Decimals

--- cryptozombies/02_chainlink_decentralized_oracles/Contract.sol
+++ cryptozombies/02_chainlink_decentralized_oracles/Contract.sol
@@ -13,9 +13,13 @@ contract PriceConsumerV3 {
     priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
   }
 
-  // Start here
   function getLatestPrice() public view returns (int) {
     (,int price,,,) = priceFeed.latestRoundData();
     return price;
+  }
+
+  function getDecimals() public view returns (uint8) {
+    uint8 decimals = priceFeed.decimals();
+    return decimals;
   }
 }
