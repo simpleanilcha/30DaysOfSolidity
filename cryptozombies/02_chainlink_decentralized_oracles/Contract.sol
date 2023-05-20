58db4aa004db8e52edd483f75e574a515274591c
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-21 05:44:04 +0545

    Constructor in a constructor

--- cryptozombies/02_chainlink_decentralized_oracles/Contract.sol
+++ cryptozombies/02_chainlink_decentralized_oracles/Contract.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.6.6;
 // 1. Import the "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol" contract
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract ZombieFactory {
+// 1. Have our `ZombieFactory` contract inherit from the `VRFConsumerBase` contract
+contract ZombieFactory is VRFConsumerbase {
 
   uint dnaDigits = 16;
   uint dnaModulus = 10 ** dnaDigits;
@@ -15,6 +16,14 @@ contract ZombieFactory {
   }
 
   Zombie[] public zombies;
+
+  // 2. Create a constructor
+    constructor() VRFConsumerBase(
+      0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF Coordinator
+      0x01BE23585060835E02B77ef475b0Cc51aA1e0709  // LINK Token
+    ) public{
+
+    }
 
   function _createZombie(string memory _name, uint _dna) private {
     zombies.push(Zombie(_name, _dna));
