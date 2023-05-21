7ec7fb273ab306b4b68751f3b9681d371a86b540
Anil Maharjan <simpleanilcha@gmail.com>
2023-05-21 05:49:29 +0545

    Define our Chaninlink VRF variables

--- cryptozombies/02_chainlink_decentralized_oracles/Contract.sol
+++ cryptozombies/02_chainlink_decentralized_oracles/Contract.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.6;
 
-// 1. Import the "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol" contract
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-// 1. Have our `ZombieFactory` contract inherit from the `VRFConsumerBase` contract
 contract ZombieFactory is VRFConsumerbase {
 
   uint dnaDigits = 16;
   uint dnaModulus = 10 ** dnaDigits;
+
+  // 1. Define the `keyHash`, `fee`, and `randomResult` variables. Don't forget to make them `public`.
+  bytes32 public keyHash;
+  uint256 public fee;
+  uint256 public randomResult;
 
   struct Zombie {
     string name;
@@ -22,7 +25,9 @@ contract ZombieFactory is VRFConsumerbase {
       0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF Coordinator
       0x01BE23585060835E02B77ef475b0Cc51aA1e0709  // LINK Token
     ) public{
-
+      // 2. Fill in the body
+      keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+      fee = 100000000000000000;
     }
 
   function _createZombie(string memory _name, uint _dna) private {
